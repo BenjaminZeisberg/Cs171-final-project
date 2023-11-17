44c98933ce2c0b30075b8da909891afa9eefbb60
Benjamin Zeisberg <benjaminzeisberg@college.harvard.edu>
2023-11-17 12:20:18 -0500

    renamed and updated logosVisualization

--- js/logosVis.js
+++ js/logosVis.js
@@ -1,4 +1,4 @@
-class GameVis {
+class LogosVis {
 
     constructor(_parentElement, _data, _teamAbbr) {
         this.parentElement = _parentElement;
@@ -22,9 +22,7 @@ class GameVis {
             .append("g")
             .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
 
-        console.log(vis.data)
         vis.wrangleData();
-        // vis.updateVis();
     }
     updateVis() {
         let vis = this;
@@ -66,52 +64,23 @@ class GameVis {
 
     wrangleData() {
         let vis = this;
-
-        // adding the winner and loser of the games as a variable per game
-        vis.data.forEach(function(game) {
-            let homeScore = parseInt(game.homeFinalScore, 10);
-            let visitorScore = parseInt(game.visitorFinalScore, 10);
-
-            if (homeScore > visitorScore) {
-                game.winner = game.homeTeamAbbr;
-                game.loser = game.visitorTeamAbbr;
-            } else if (visitorScore > homeScore) {
-                game.winner = game.visitorTeamAbbr;
-                game.loser = game.homeTeamAbbr;
-            } else {
-                game.winner = 'Tie';
-                game.loser = 'Tie';
-            }
-        });
+        //
+        // // adding the winner and loser of the games as a variable per game
+        // vis.data.forEach(function(game) {
+        //     let homeScore = parseInt(game.homeFinalScore, 10);
+        //     let visitorScore = parseInt(game.visitorFinalScore, 10);
+        //
+        //     if (homeScore > visitorScore) {
+        //         game.winner = game.homeTeamAbbr;
+        //         game.loser = game.visitorTeamAbbr;
+        //     } else if (visitorScore > homeScore) {
+        //         game.winner = game.visitorTeamAbbr;
+        //         game.loser = game.homeTeamAbbr;
+        //     } else {
+        //         game.winner = 'Tie';
+        //         game.loser = 'Tie';
+        //     }
+        // });
         vis.updateVis();
-        console.log(vis.data)
     }
-    //
-    // updateVis() {
-    //     let vis = this
-    //
-    //     let logoWidth = 250; // Adjust logo width as needed
-    //     let logoSpacing = (vis.width - (logoWidth * vis.teams.length)) / (vis.teams.length + 1); // Dynamic spacing
-    //
-    //     vis.teams.forEach(function(teamAbbr, index) {
-    //         let xPosition = logoSpacing + index * (logoWidth + logoSpacing); // Dynamically calculate x position
-    //         let yPosition = 50; // Adjust vertical position as needed
-    //
-    //         // For the hover we create an underlying circle for the logo
-    //         vis.svg.append("circle")
-    //             .attr("cx", xPosition) // Center of the logo
-    //             .attr("cy", yPosition) // Center of the logo
-    //             .attr("r", logoWidth / 2) // Radius to cover the logo
-    //             .style("fill", "transparent")
-    //             .style("cursor", "pointer"); // Change cursor on hover to indicate interactivity
-    //
-    //         vis.svg.append("image")
-    //             .attr("xlink:href", "data/logosWeb/" + teamAbbr + ".webp") // Assuming the logo files are named after the team abbreviations
-    //             .attr("x", xPosition)
-    //             .attr("y", yPosition)
-    //             .attr("width", 50) // Set your desired width
-    //             .attr("height", 50); // Set your desired height
-    //     });
-    // }
-
 }
