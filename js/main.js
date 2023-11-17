78cc900c59d9afa1987505a3b29342086f0c3be1
Benjamin Zeisberg <benjaminzeisberg@college.harvard.edu>
2023-11-17 12:20:38 -0500

    changed structure to include first diagram visualization

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ let dateParser = d3.timeParse("%Y-%m-%d");
 
 let promises = [
     d3.csv("data/games.csv"),
-    d3.csv("data/players.csv"),
-    d3.csv("data/plays.csv"),
-    d3.csv("data/tackles.csv"),
-    d3.csv('data/tracking_week_1.csv'),
+
+    // To spead up development we only include the games promise
+    // d3.csv("data/players.csv"),
+    // d3.csv("data/plays.csv"),
+    // d3.csv("data/tackles.csv"),
+    // d3.csv('data/tracking_week_1.csv'),
 ];
 
 Promise.all(promises)
@@ -29,9 +31,11 @@ function createVis(data) {
     // let teams = data[3]
     // let trackingWeek1 = data[4]
 
+    // console.log(games)
     // It's faster to give as an array as supposed to looping through the lists each time the website is opened
     let teamsAbbr = ['LA', 'BUF', 'ATL', 'NO', 'CAR', 'CLE', 'CHI', 'SF', 'CIN', 'PIT', 'DET', 'PHI', 'HOU', 'IND', 'MIA', 'NE', 'NYJ', 'BAL', 'WAS', 'JAX', 'ARI', 'KC', 'LAC', 'LV', 'MIN', 'GB', 'TEN', 'NYG', 'DAL', 'TB', 'SEA', 'DEN']
 
     // Creating positional Game Visualization.
-    let gameVis = new GameVis("gameVis", games, teamsAbbr);
+    let logosVis = new LogosVis("logosVis", games, teamsAbbr);
+    let diagramVis = new DiagramVis("diagramVis", games, teamsAbbr);
 }
