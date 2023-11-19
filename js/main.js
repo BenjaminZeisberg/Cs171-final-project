f84c7c6e75064f31951f163f584427556617eac7
Benjamin Zeisberg <benjaminzeisberg@college.harvard.edu>
2023-11-19 15:53:46 -0500

    added a new Visualization

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let dateFormatter = d3.timeFormat("%Y-%m-%d");
 let dateParser = d3.timeParse("%Y-%m-%d");
 
 // Declaring global variables
-let diagramVis;
+let diagramVis, winsTime;
 
 // Step 1 Load data using promises
 
@@ -28,11 +28,15 @@ Promise.all(promises)
 // Creating the Vis
 function createVis(data) {
     let games = data[0]
-    // let players = data[1]
-    // let plays = data[2]
-    // let teams = data[3]
-    // let trackingWeek1 = data[4]
-
+    let players = data[1]
+    let plays = data[2]
+    let teams = data[3]
+    let trackingWeek1 = data[4]
+    console.log(games)
+    // console.log(players)
+    // console.log(plays)
+    // console.log(teams)
+    // console.log(trackingWeek1)
     // console.log(games)
     // It's faster to give as an array as supposed to looping through the lists each time the website is opened
     let teamsAbbr = ['LA', 'BUF', 'ATL', 'NO', 'CAR', 'CLE', 'CHI', 'SF', 'CIN', 'PIT', 'DET', 'PHI', 'HOU', 'IND', 'MIA', 'NE', 'NYJ', 'BAL', 'WAS', 'JAX', 'ARI', 'KC', 'LAC', 'LV', 'MIN', 'GB', 'TEN', 'NYG', 'DAL', 'TB', 'SEA', 'DEN']
@@ -40,13 +44,11 @@ function createVis(data) {
     // Creating positional Game Visualization.
     let logosVis = new LogosVis("logosVis", games, teamsAbbr);
     diagramVis = new DiagramVis("diagramVis", games, teamsAbbr);
+    winsTime = new WinsVis("winsTime", games, teamsAbbr);
 }
 
-function handleLogoClick() {
+function handleLogoClick(teamAbbr) {
     // Highlighting the selected team
     console.log(diagramVis)
-    let teamAbbr = 'LA'
-    // diagramVis.highlightTeam(teamAbbr);
     diagramVis.highlightTeam(teamAbbr);
-    // diagramVis.updateVis();
 }
