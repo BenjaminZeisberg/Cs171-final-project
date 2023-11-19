215d6f4cef3974fd72f81d7c8103109554104cf1
jamesgenie <james.lockowandt@getgenie.io>
2023-11-19 13:37:14 -0500

    my first commit to the repo

---
+++ js/james.js
@@ -0,0 +1,10 @@
+class jamesVis {
+    constructor(_parentElement, _data, _teamAbbr) {
+        this.parentElement = _parentElement;
+        this.data = _data;
+        this.teams = _teamAbbr;
+        this.filteredData = this.data;
+
+        this.initVis();
+    }
+}
